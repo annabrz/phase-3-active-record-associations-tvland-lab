be50b8ead8551ce44b6c3a3c91854130c4e4be2b
AnnaAyvazyan <ayvazyananna@yahoo.com>
2022-09-08 00:56:37 -0700

    commit tree 9fe0b8d7dda33257a1f441488d7accb59362d5ba

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+##ASK ABOUT THIS:(is there a dif way to do this, or pluck)
+  def list_roles
+    self.characters.map do |character|
+        "#{character.name} - #{character.show.name}"
+    end
+  end
 end
